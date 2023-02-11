0105a2e54896a5875558807b0b015a327d9a783d
Alex <120931620+clubbars79@users.noreply.github.com>
2023-02-11 19:52:36 +0300

    Add files via upload

---
+++ Задача66.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число M : ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число N : ");
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+void SumNaturalNumbers(int m, int n)
+{
+    if (m > n)
+    {
+        return;
+    }
+    sum = sum + m;
+   
+    SumNaturalNumbers(m + 1, n);
+}
+SumNaturalNumbers(m,n);
+ Console.WriteLine("Сумма всех натуральных чисел в промежутке от " + m + " до " + n + " = " + sum);
---
+++ Задача68.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число N : ");
+uint n = uint.Parse(Console.ReadLine());
+Console.Write("Введите число M : ");
+uint m = uint.Parse(Console.ReadLine());
+Console.Write("A(" + n + "," + m + ")");
+uint s = 0;
+A(n,m);            
+            uint A(uint n, uint m)
+            {
+                if (n == 0) return s = m+1;
+                if (n != 0 && m == 0) return A(n - 1, 1);
+                if (n > 0 && m > 0) return A(n - 1, A(n, m - 1));
+                return A(n,m);
+            }
+Console.Write(s);
+
+ 
